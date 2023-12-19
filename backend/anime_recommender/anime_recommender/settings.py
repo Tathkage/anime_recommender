82b35ea14674ead3da6a50ba6a19a0d2a9c8007c
Tathluach Chol <tathluach.krc@gmail.com>
2023-12-19 01:36:46 -0500

    Updated settigns.py
    Removed leaked django secret key.

--- backend/anime_recommender/anime_recommender/settings.py
+++ backend/anime_recommender/anime_recommender/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dy-m8-iu8w8ki^u15+8@-si=769miu09w30d^uibk2^ehmm+*g'
+SECRET_KEY = 'DJANGO_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
