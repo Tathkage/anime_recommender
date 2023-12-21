98348f9aa4e5e69ba50d2b49c53dcbfcc9ae5bc8
Tathluach Chol <tathluach.krc@gmail.com>
2023-12-21 17:06:31 -0500

    Backend scraping now used in frontend

--- frontend/anime-recommender/src/app/anime-list/anime-list.component.ts
+++ frontend/anime-recommender/src/app/anime-list/anime-list.component.ts
@@ -1,23 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 import { AnimeService } from '../services/anime.service';
+
+interface Anime {
+  Title: string;
+  Rating: string;
+}
+
+interface AnimeData {
+  'Anime Info': Anime[];
+}
 
 @Component({
   selector: 'anime-list',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HttpClientModule], // Add HttpClientModule here
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './anime-list.component.html',
-  styleUrls: ['./anime-list.component.css'] // Correct 'styleUrl' to 'styleUrls'
+  styleUrls: ['./anime-list.component.css']
 })
+
 export class AnimeListComponent implements OnInit {
-  animeList: any[] = [];
+  animeList: Anime[] = [];
 
   constructor(private animeService: AnimeService) {}
 
   ngOnInit(): void {
-    this.animeService.getAnimeList().subscribe(data => {
+    this.animeService.getAnimeList().subscribe((data: AnimeData) => {
       this.animeList = data['Anime Info'];
     });
   }
--- frontend/anime-recommender/src/main.ts
+++ frontend/anime-recommender/src/main.ts
@@ -1,13 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
 import { withFetch } from '@angular/common/http';
 
+// Assuming appConfig is an object containing necessary configurations
+import { appConfig } from './app/app.config';
+
 bootstrapApplication(AppComponent, {
   ...appConfig,
   providers: [
-    ...appConfig.providers,
+    ...(appConfig.providers || []),
     provideHttpClient(withFetch()),
   ],
 }).catch((err) => console.error(err));
