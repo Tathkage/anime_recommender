f5142de02f89c5af14dc4b2c04db8eea61bc38e2
Tathluach Chol <tathluach.krc@gmail.com>
2023-12-23 23:46:23 -0500

    CR in CRUD for Watchlist
    A watchlist can be created and a user can see their watchlist titles. However, a user cannot update or delete a watchlist, yet.

--- backend/anime_recommender/anime_recommender/settings.py
+++ backend/anime_recommender/anime_recommender/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
+    'rest_framework.authtoken'
 ]
 
 MIDDLEWARE = [
@@ -134,3 +136,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
